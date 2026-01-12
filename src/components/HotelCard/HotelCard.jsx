d271572abfdb6fd114c5c522e0503332ddf591e8
Inder <kumarinder799@gmail.com>
2026-01-12 16:50:35 +0530

    Home page via api and infinite scroll implementation

--- src/components/HotelCard/HotelCard.jsx
+++ src/components/HotelCard/HotelCard.jsx
@@ -1,25 +1,24 @@
 import "./HotelCard.css";
 
-export const HotelCard = () => {
+export const HotelCard = ({ hotel }) => {
+  const { _id, name, image, address, state, rating, price } = hotel;
   return (
     <div className="hotelcard-container shadow cursor-pointer relative">
       <div className="">
-        <img
-          className="img"
-          src="https://a0.muscache.com/im/pictures/miso/Hosting-20815828/original/f4cfbfab-6113-444b-b1ac-f60e72ea510d.jpeg?im_w=1200"
-          alt="hotelcard"
-        />
+        <img className="img" src={image} alt={name} />
         <div className="hotelcard-details">
           <div className="d-flex align-center">
-            <span className="location">Bir, Himachal Pradesh</span>
+            <span className="location">
+              {address}, {state}
+            </span>
             <span className="rating d-flex align-center">
               <span class="material-icons-outlined">star</span>
-              <span>4.3</span>
+              <span>{rating}</span>
             </span>
           </div>
-          <p className="hotel-name">Sukoon Bag</p>
+          <p className="hotel-name">{name}</p>
           <p className="price-details">
-            <span className="price">Rs. 3500</span>
+            <span className="price">Rs. {price}</span>
             <span>Night</span>
           </p>
         </div>
